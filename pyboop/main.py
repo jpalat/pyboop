5b9bcf50bc3daeec57c2f5f8a68b0bf01792f703
Jay Palat <jay@palat.net>
2023-03-13 08:21:43 -0400

    Add kitten to board.

--- pyboop/main.py
+++ pyboop/main.py
@@ -11,12 +11,22 @@ class Boop:
     def __init__(self):
         self.board = [['.','.','.','.','.','.'],['.','.','.','.','.','.'],['.','.','.','.','.','.'],['.','.','.','.','.','.'],['.','.','.','.','.','.'],['.','.','.','.','.','.']]
 
-    
     def drawBoard(self):
+        print("-------")
         for i in self.board:
             for j in i:
                 print(j, end=" ")
             print("\n")
+
+    
+    def placeKitten(self, x, y):
+        if (x > 0) and (x < 6) and (y > 0) and (y < 6):
+            if self.board[x][y] == '.':
+                self.board[x][y] = 'k'
+                self.drawBoard()
+                return
+        print("Error placing kitten.")
+            
 
 # Define the main function
 def main(args):
@@ -24,6 +34,9 @@ def main(args):
     print(f'Running boopy with input file "{args.input}" and output file "{args.output}"')
     boop = Boop()
     boop.drawBoard()
+    boop.placeKitten(x=1, y=1)
+    boop.placeKitten(x=1, y=2)
+    boop.placeKitten(x=1, y=1)
 
 # Call the main function with command-line arguments
 if __name__ == '__main__':
