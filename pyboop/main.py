d1449f8890799e37087ebce7086e8123773540d8
Jay Palat <jay@palat.net>
2023-03-14 23:32:24 -0400

    Implement boop for kitten.
    Add a piece. If there is a neigboring kitten, move the kitten.

--- pyboop/main.py
+++ pyboop/main.py
@@ -16,6 +16,7 @@ class Player:
 class Boop:
     def __init__(self):
         self.board = [['.','.','.','.','.','.'],['.','.','.','.','.','.'],['.','.','.','.','.','.'],['.','.','.','.','.','.'],['.','.','.','.','.','.'],['.','.','.','.','.','.']]
+        
 
     def drawBoard(self):
         print("-------")
@@ -30,8 +31,43 @@ class Boop:
             if self.board[x][y] == '.':
                 self.board[x][y] = 'k'
                 self.drawBoard()
-                return
+                self.boop(x,y)
+                return 1
         print("Error placing kitten.")
+        return -1
+    
+    def boop(self, newx, newy):
+        # mutate board with new kitten.
+        xpos = [-1,0,1]
+        ypos = [-1,0,1]
+        for x in xpos:
+            for y in ypos:
+                if x == 0 and y == 0:
+                    print('self')
+                    
+                else:
+                    validation_x = newx + x
+                    validation_y = newy + y
+                    if self.board[validation_x][validation_y] == 'k':
+                        print('Boop', validation_x, validation_y)
+                        self.boop_move(origin_x=validation_x, origin_y=validation_y, off_x=x, off_y=y)
+
+    
+    def boop_move(self, origin_x, origin_y, off_x, off_y):
+        x = off_x + origin_x
+        y = off_y + origin_y
+        print ('move: ', origin_x, origin_y, 'to', x, y, 'os', off_x, off_y)
+        if x < 0 or x > 6 or y < 0 or y > 6:
+            print('return kitten')
+        else:
+            self.board[x][y] = 'k'
+        self.board[origin_x][origin_y] = '.'
+        self.drawBoard()
+        
+
+                
+        
+
             
 
 # Define the main function
@@ -40,9 +76,12 @@ def main(args):
     print(f'Running boopy with input file "{args.input}" and output file "{args.output}"')
     boop = Boop()
     boop.drawBoard()
+    print('place 1,1')
     boop.placeKitten(x=1, y=1)
+    print('place 1,2')
     boop.placeKitten(x=1, y=2)
     boop.placeKitten(x=1, y=1)
+    boop.placeKitten(x=2, y=2)
 
 # Call the main function with command-line arguments
 if __name__ == '__main__':
