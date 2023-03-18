abf4f32d6a7374874a48a0b18fd7c00c4676a5b6
Jay Palat <jay@palat.net>
2023-03-17 22:54:49 -0400

    More tests.
    Test to place kitten and boop.

--- tests/tests.py
+++ tests/tests.py
@@ -1,19 +1,41 @@
 import unittest
+import copy
 from pyboop.main import Boop
 
 class BoopTests(unittest.TestCase):
 
-    def test_place(self):
-        newboard = [['.', '.', '.', '.', '.', '.'], 
+    def setUp(self) -> None:
+        self.newboard = [['.', '.', '.', '.', '.', '.'], 
                     ['.', '.', '.', '.', '.', '.'], 
                     ['.', '.', '.', '.', '.', '.'], 
                     ['.', '.', '.', '.', '.', '.'], 
                     ['.', '.', '.', '.', '.', '.'], 
                     ['.', '.', '.', '.', '.', '.']]
+
+    def test_init(self):
         boop = Boop()
-        self.assertEqual(boop.board, newboard, "board init.")
-
-
+        self.assertEqual(boop.board, self.newboard, "board fail.")
+    def test_place(self):
+        boop = Boop()
+        board = copy.deepcopy(self.newboard)
+        board[1][1] = 'k'
+        boop.placeKitten(x=1, y=1)
+        self.assertEqual(boop.board, board, "misplaced kitten")
+    def test_place_and_boop(self):
+        boop = Boop()
+        board = copy.deepcopy(self.newboard) 
+        boop.placeKitten(x=1, y=1)
+        boop.placeKitten(x=1, y=2)
+        board[1][2] = 'k'
+        board[1][0] = 'k'
+        self.assertEqual(boop.board, board, "moved kitten")
+    def test_place_and_boop(self):
+        boop = Boop()
+        board = copy.deepcopy(self.newboard) 
+        boop.placeKitten(x=0, y=1)
+        boop.placeKitten(x=1, y=1)
+        board[1][1] = 'k'
+        self.assertEqual(boop.board, board, "moved kitten")
 
 if __name__ == '__main__':
     unittest.main()
