e4041aec558c86bfbb07718a1c1a26db76466568
Jay Palat <jay@palat.net>
2023-04-24 11:09:54 -0400

    Add tests for directional boops.

--- tests/tests.py
+++ tests/tests.py
@@ -34,21 +34,35 @@ class BoopTests(unittest.TestCase):
         board[1][1] = 'k'
         boop.placeKitten(x=1, y=1)
         self.assertEqual(boop.board, board, "misplaced kitten")
-    def test_place_and_boop(self):
-        boop = Boop()
-        board = copy.deepcopy(self.newboard) 
+    def test_place_small(self):
+        boop = Boop(3)
+        board = [['.', '.', '.'], 
+                    ['.', '.', '.'], 
+                    ['.', '.', '.']]
+        board[1][1] = 'k'
         boop.placeKitten(x=1, y=1)
-        boop.placeKitten(x=1, y=2)
-        board[1][2] = 'k'
-        board[1][0] = 'k'
-        self.assertEqual(boop.board, board, "moved kitten")
+        self.assertEqual(boop.board, board, "misplaced kitten")
     def test_place_and_boop(self):
-        boop = Boop()
-        board = copy.deepcopy(self.newboard) 
+        boop = Boop(3)
+        boop.drawBoard()
+        board = [['.', '.', '.'], 
+                 ['.', '.', '.'],
+                 ['.', '.', '.']]
+        boop.placeKitten(x=1, y=1)
+        boop.placeKitten(x=0, y=1)
+        board[0][1] = 'k'
+        board[2][1] = 'k'
+        self.assertEqual(boop.board, board, "failed to boop kitten")
+    def test_place_with_edge(self):
+        boop = Boop(3)
+        boop.drawBoard()
+        board = [['.', '.', '.'], 
+                 ['.', '.', '.'],
+                 ['.', '.', '.']]
         boop.placeKitten(x=0, y=1)
         boop.placeKitten(x=1, y=1)
         board[1][1] = 'k'
-        self.assertEqual(boop.board, board, "moved kitten")
+        self.assertEqual(boop.board, board, "failed to boop kitten")
 
 if __name__ == '__main__':
     unittest.main()
